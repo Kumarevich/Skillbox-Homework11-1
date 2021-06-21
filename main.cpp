f5df0c93f5307abe1a31c2c9d8ba1791c1633d02
Igor Makarevich <igor.makarevich@novometgroup.com>
2021-06-21 16:21:20 +0500

    Fixed the shift calculation

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <string>
 
-int shift_char (int min, int max, int shift, char a) {
+int shift_char (int max, int shift, char ch) {
+    int min = max - 25;
+    int a = ch;
     a += shift;
     if (a > max) {
-        a = min + (a - max);
-    } else if (a < min) {
-        a = max - (min - a);
+        a = min + ((a - max) % 25);
+    }
+    else if (a < min) {
+        a = max - ((min - a) % 25);
     }
     return a;
 }
 
 int char_type (char ch) {
-    if (ch >= 65 && ch <= 90) {
-        return 90;
-    } else if (ch >= 97 && ch <= 122) {
-        return 122;
+    if (ch >= 'A' && ch <= 'Z') {
+        return 'Z';
+    } else if (ch >= 'a' && ch <= 'z') {
+        return 'z';
     } else {
         return 0;
     }
@@ -25,14 +28,15 @@ std::string encrypt_caesar(std::string str, int n) {
     int l = str.length() - 1;
     for (int i = 0; i <= l; ++i) {
         if (char_type(str[i]) != 0) {
-            str[i] = shift_char((char_type(str[i]) - 25), char_type(str[i]), n, str[i]);
+            str[i] = shift_char(char_type(str[i]), n, str[i]);
         }
     }
     return str;
 }
 
 std::string decrypt_ceasar(std::string str, int n) {
-    return encrypt_caesar(str, -n);
+    n = -n;
+    return encrypt_caesar(str, n);
 }
 
 int main() {
